b6acf7f32c07a2cb19c9a696eaa7607b2cdf5e1e
Kim <tmmd0308@gmail.com>
2019-03-02 20:50:31 +0900

    CORS 관련 문제 수정

--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const errorTemplate = require('../common/error_template');
 
@@ -34,14 +35,10 @@ module.exports = () => {
         useNewUrlParser: true
     });
 
+    app.use(cors());
+
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
-
-    app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        next();
-    });
 
     app.use("/user", require('./user'));
 
@@ -49,10 +46,10 @@ module.exports = () => {
         const statusCode = err.statusCode || 500;
         res.status(statusCode).json(errorTemplate(statusCode, err.message));
     });
-
     app.use((req, res, next) => {
         res.status(404).json(errorTemplate(404, "Page not found"))
     });
+
 
     app.listen(PORT, () => {
         console.log(`App listens port ${PORT}`);
