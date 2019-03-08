691a806d1a66e9234ad29b4cd1757f7cda032679
Doyeong Kim <jeekc0308@gmail.com>
2019-03-08 17:02:50 +0900

    코드 인증, 생성 구형

--- routers/user/auth.js
+++ routers/user/auth.js
@@ -9,13 +9,56 @@ const userSchema = require('../../models/user');
 
 const jwt_secret = process.env.JWT_SECRET || "Adgdsg#ddgdg45ty4wefwefwe9jfpgpnweg3DWGwdgtg42weGWEDgv24g";
 
+function verifyToken(token) {
+    if (!token) return false;
+    try {
+        JWT.verify(token, jwt_secret);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+function getRandomString(len = 16) {
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let tmp = "";
+    for (let i = 0; i < len; i++) {
+        tmp += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return tmp;
+}
 // /user/auth/code/:code, 코드 검증 부분. 
 // 현재는 임시로 모두 true 반환
 router.get('/code/:code', (req, res) => {
-    res.json({
-        success: true
-    });
+    codeSchema.findOne({
+        code: req.params.code
+    })
+    .then(value => {
+        if (!value) {
+            return res.status(403).json(error_template(403, "인증 코드가 없습니다."));
+        }
+        res.json({
+            success: true
+        });
+    })
 });
+
+// /user/auth/code 코드를 발급받는 부분.
+router.post('/code', (req, res) => {
+    const accessToken = req.headers['x-access-code'];
+    if (!accessToken) {
+        return res.status(403).json(error_template(403, "토큰이 필요합니다."));
+    }
+    const code = getRandomString(24);
+    codeSchema.create({
+        code
+    })
+    .then(value => {
+        res.status(202).json({
+            success: true,
+            code
+        });
+    });
+})
 
 // /user/auth, JWT 토큰 발급 부분.
 router.post('/', (req, res) => {
@@ -59,14 +102,9 @@ router.post('/', (req, res) => {
 // /user/auth/validate, JWT 검증
 router.get('/validate', (req, res) => {
     const token = req.headers['x-access-token'];
-    if (!token) {
-        return res.status(403).json(error_template(403, "토큰이 필요합니다."));
+    if (verifyToken(token)) {
+        return res.status(200).json({success: true});
     }
-    try {
-        let verify = JWT.verify(token, jwt_secret);
-        res.status(200).json({success: true, decoded: verify});
-    } catch (e) {
-        return res.status(403).json(error_template(403, "토큰이 유효하지 않습니다."));
-    }
-})
+    return res.status(403).json(error_template(403, "토큰이 유효하지 않습니다."));
+});
 module.exports = router;
